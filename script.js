fa39a0451dd3950a0bf3c845cf91f979a7a5165f
rchalaaa <r.chalabyan13@gmail.com>
2024-06-13 11:07:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -116,18 +116,11 @@ function startGame() {
 
 function displayPlayerRole() {
     playersRef.child(playerId).child('role').on('value', (snapshot) => {
-      const role = snapshot.val();
-      const playerInfoElement = document.getElementById('player-info');
-      
-      if (role === 'Blanco') {
-        playerInfoElement.innerText = 'Blanco';
-        playerInfoElement.classList.add('blanco');
-      } else {
-        playerInfoElement.innerText = role;
-        playerInfoElement.classList.remove('blanco');
-      }
+        const role = snapshot.val();
+        const playerInfo = document.getElementById('player-info');
+        playerInfo.innerText = `${role === 'Blanco' ? 'Blanco' : role}`;
     });
-  }
+}
 
 gameRef.child('status').on('value', (snapshot) => {
     const status = snapshot.val();
